47e0ee2ae0eeda0eb4838883208f8b80b1a33c62
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-09-18 21:55:45 +0700

    Create city model

---
+++ CitiesManager.WebAPI/Models/City.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CitiesManager.WebAPI.Models
+{
+    public class City
+    {
+        [Key]
+        public Guid CityId { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        public string? CityName { get; set; }
+    }
+}
